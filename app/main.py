c6c314e58cdc468b3b2ce6d9467f06cdfb057fdd
Megha Arya <meghanitk2026@gmail.com>
2025-05-27 23:34:04 +0530

    exit command

--- app/main.py
+++ app/main.py
@@ -11,7 +11,13 @@ def main():
         sys.stdout.write("$ ")
         command = input()
         print(f"{command}: command not found")
-    
+
+
+        if command.strip() == "exit(0)":
+            sys.exit(0)  # or just use exit(0)
+
+        if command[:4] == "exit":
+            sys.exit(0)
 
 if __name__ == "__main__":
     main()
